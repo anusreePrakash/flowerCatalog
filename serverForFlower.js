eea210e4eb300afd06c67ca24256cfa44cf6207e
anusree <anusree@INanusree.local>
2015-10-31 10:06:14 +0530

    serverForFlower is modified file

--- serverForFlower.js
+++ serverForFlower.js
@@ -1,16 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 
-var handleRequest = function(request,response){
-	var loadImage = request.url;
-	
-	var file=fs.readFileSync("/Users/anusree/my_webpage.html/flower/flowercatalog.html")
-	response.write(file);
-
-	response.end();
+var display = function(url){
+	var file = url == "/" ? "./flowercatalog.html" : "." +url;
+	var content = fs.readFileSync(file);
+	return content;
 }
 
+var handleRequest = function(request,response){
+	console.log(request.url);
+	// var loadImage = request.url;
+	
+	// var file=fs.readFileSync("/Users/anusree/my_webpage.html/flower/flowercatalog.html")
+	// response.write(file);
 
-
+	response.end(display(request.url));
+};
 var server = http.createServer(handleRequest);
-server.listen(4040);
+server.listen(4004)
